e24891454b9d30bc1aa0b149edddd8e07dddecd1
DonC24 <51435215+DonC24@users.noreply.github.com>
2021-05-13 17:53:22 +0800

    basic call from api

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,46 @@
 #!/usr/bin/env node
 
-console.log( "Hello!" );
+const yargs = require("yargs");
+const axios = require("axios");
+
+const options = yargs
+ .usage("Usage: -s <pokemon name or id>")
+ .option("s", { alias: "search", describe: "Search term", type: "string" })
+ .argv;
+
+const greet = `Hello!`;
+
+console.log(greet);
+
+if (options.search) {
+    console.log(`Searching for pokemon ${options.search}...`)
+} else {
+    console.log("Here's a pokemon for you:");
+}
+
+const url = options.search ? `https://pokeapi.co/api/v2/pokemon/${options.search}` : "https://pokeapi.co/api/v2/pokemon/ditto";
+console.log(url);
+
+axios.get(url, { headers: { Accept: "application/json" } })
+    .then(res => {
+        if (options.search) {
+            /*console.log(res);*/
+                console.log("\n Pokemon ID: " + res.data.id);
+                console.log("\n Pokemon Name: " + res.data.name);
+                console.log("\n Pokemon Types: ");
+                res.data.types.forEach( i => {
+                    console.log(" " + i.type.name);
+                });
+        } else {
+            console.log("\n Pokemon ID: " + res.data.id);
+            console.log("\n Pokemon Name: " + res.data.name);
+            console.log("\n Pokemon Types: ");
+            res.data.types.forEach( i => {
+                console.log(" " + i.type.name);
+            });
+        }
+    })
+    .catch(function (error) {
+        // handle error
+        console.log("no Pokemon found.");
+    });
