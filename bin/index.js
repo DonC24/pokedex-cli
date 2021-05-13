644497a43ab8775db1c813bb694ac49636a89d92
DonC24 <51435215+DonC24@users.noreply.github.com>
2021-05-13 18:48:26 +0800

    Added encounters and location

--- bin/index.js
+++ bin/index.js
@@ -25,12 +25,17 @@ axios.get(url, { headers: { Accept: "application/json" } })
     .then(res => {
         if (options.search) {
             /*console.log(res);*/
-                console.log("\n Pokemon ID: " + res.data.id);
-                console.log("\n Pokemon Name: " + res.data.name);
-                console.log("\n Pokemon Types: ");
-                res.data.types.forEach( i => {
-                    console.log(" " + i.type.name);
-                });
+            console.log("\n Pokemon ID: " + res.data.id);
+            console.log("\n Pokemon Name: " + res.data.name);
+            console.log("\n Pokemon Types: ");
+            res.data.types.forEach( i => {
+                console.log(" " + i.type.name);
+            });
+            console.log("\n Pokemon Stats: ");
+            res.data.stats.forEach( i => {
+                console.log(" " + i.stat.name);
+                console.log("  Base stat: " + i.base_stat);
+            });
         } else {
             console.log("\n Pokemon ID: " + res.data.id);
             console.log("\n Pokemon Name: " + res.data.name);
@@ -38,7 +43,32 @@ axios.get(url, { headers: { Accept: "application/json" } })
             res.data.types.forEach( i => {
                 console.log(" " + i.type.name);
             });
+            console.log("\n Pokemon Stats: ");
+            res.data.stats.forEach( i => {
+                console.log(" " + i.stat.name);
+                console.log("  Base stat: " + i.base_stat);
+            });
         }
+        return res.data.id;
+    })
+    .then(info => { return axios.get(`https://pokeapi.co/api/v2/pokemon/${info}/encounters`)})
+    .then(res2 => {
+        res2.data.forEach( i => {
+                let loc_name = i.location_area.name;
+                let loc_kan = loc_name.search("kanto");
+                if (loc_kan > -1) {
+                    console.log("\n Location Area Name: " + i.location_area.name);
+                    i.version_details.forEach( j => {
+                        console.log("\n Version: " + j.version.name);
+                        j.encounter_details.forEach( k => {
+                            console.log("\n   Method: ")
+                            console.log("   " + k.method.name);
+                        })
+                    })
+                } else {
+                    console.log("-");
+                }
+            });
     })
     .catch(function (error) {
         // handle error
